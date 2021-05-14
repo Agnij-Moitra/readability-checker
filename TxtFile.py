2f497774af69cb70553f38b5851407e2f7a7a7bf
Agnij-Moitra <79319209+Agnij-Moitra@users.noreply.github.com>
2021-05-14 12:04:20 +0530

    Add files via upload

---
+++ TxtFile.py
@@ -0,0 +1,27 @@
+import sys
+import importlib.util
+
+
+def main(file_path):
+    # importing readability.py
+    readability = load_module("readability-algo.py", "main")
+
+    # rasing error
+    try:
+        # opening the given file
+        with open(file_path, 'r') as txtf:
+            text = txtf.read().replace("\n", " ")
+            readindex = readability.main(text)
+            return readindex
+
+    except FileNotFoundError:
+        return "404: File Not Found"
+
+
+def load_module(file_name, module_name):
+
+    spec = importlib.util.spec_from_file_location(module_name, file_name)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    return module
